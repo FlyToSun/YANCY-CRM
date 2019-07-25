556bf3bebde89345c9fb0feabce08d478aa50a49
FlyToSun <lyxican@qq.com>
2019-07-25 09:43:45 +0800

    第一次提交

---
+++ boot-crm/src/com/itheima/core/service/BaseDictService.java
@@ -0,0 +1,15 @@
+package com.itheima.core.service;
+
+import java.util.List;
+
+import com.itheima.core.po.BaseDict;
+
+/**
+ * 数据字典Service接口
+ * @author Yancy
+ *
+ */
+public interface BaseDictService {
+	//根据类别代码查询数据字典
+	public List<BaseDict> findBaseDictByTypeCode(String typecode);
+}
